54ab8045c948811dcb90bb1a29484f5a66e40558
rahul ☕ <rahulmg05@gmail.com>
2025-04-27 12:39:31 -0500

    adding the bells and whistles

---
+++ src/test/java/dev/rahulmg/snipshare/SnipshareApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rahulmg.snipshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnipshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
